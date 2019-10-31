186bf257bd7450049bba4527af942ef8891aac33
Mikhail Salavei <soloveym@famcs-steps.yaconnect.com>
2019-10-31 15:29:35 +0300

    add second blank activity and landscape orientation

---
+++ GOGOT/app/src/main/java/com/example/gogot/GameBoardActivity.java
@@ -0,0 +1,13 @@
+package com.example.gogot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class GameBoardActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- GOGOT/app/src/main/java/com/example/gogot/MainActivity.java
+++ GOGOT/app/src/main/java/com/example/gogot/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gogot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button newGameButton = findViewById(R.id.buttonNewGame);
+        newGameButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,
+                    GameBoardActivity.class);
+            startActivity(intent);
+        });
     }
 }
