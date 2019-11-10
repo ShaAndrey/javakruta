eb4bcb582d33222350725403bc2f255fdc8bd45a
Mikhail Salavei <soloveym@famcs-steps.yaconnect.com>
2019-11-10 21:26:05 +0300

    add player's hand base logic

--- GOGOT/app/src/main/java/com/example/gogot/model/PlayersHand.java
+++ GOGOT/app/src/main/java/com/example/gogot/model/PlayersHand.java
@@ -4,4 +4,16 @@ import java.util.ArrayList;
 
 abstract public class PlayersHand {
     protected ArrayList<PlayCard> inHandCards;
+
+    PlayersHand() {
+        inHandCards = new ArrayList<PlayCard>();
+    }
+
+    void addCardToHand(PlayCard card) {
+        inHandCards.add(card);
+    }
+
+    void removeCardFromHand(PlayCard card) {
+        inHandCards.remove(card);
+    }
 }
